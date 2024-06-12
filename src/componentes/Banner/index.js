fa82f9f5ec52b1b30386b4c19d0dfb3605243ef1
Jhony Renan Cupriva dos Santos <jhony.renan777@gmail.com>
2024-06-11 21:41:57 -0300

    First Commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,10 @@
+import "./index.css";
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página do Organo"></img>
+        </header>
+    )
+}
+export default Banner
---
+++ src/componentes/Botao/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+const Botao = (props) => {
+    return (
+        <button className="botao">{props.texto}</button>
+    )
+}
+
+export default Botao
